7e73e9e2f1c7abb41fff0beff79752639ae34d61
Thomas chacko <165127786+thomas-stratagile@users.noreply.github.com>
2026-01-01 12:31:19 +0530

    layout clean

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Provider from "./Provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,9 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Provider>
-          {children}
-        </Provider>
+        {children}
       </body>
     </html>
   );
