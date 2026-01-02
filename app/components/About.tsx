5962693aeffdb8a20f214d8c0083af67b765c7b7
Thomas chacko <165127786+thomas-stratagile@users.noreply.github.com>
2026-01-02 17:24:44 +0530

    about section skelton

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,6 +1,6 @@
 export const About = () => {
     return (
-        <section>
+        <section className="min-h-screen items-center justify-center border-t border-white/10 bg-neutral-900 px-6 py-24">
             About
         </section>
     )
